91ae67bdc2434cd42dc7c1f22d4de878ef7dd86f
Shahna Nasrin <shahnanasrin002@gmail.com>
2026-07-20 16:02:59 +0530

    Fix Nodemailer IPv6 routing on Render

--- server.js
+++ server.js
@@ -46,10 +46,15 @@ function generateOTP() {
 
 // Nodemailer setup
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
